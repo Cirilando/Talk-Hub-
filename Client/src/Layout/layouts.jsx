0202dea9f6df042fd527341162b23a536bbdb326
Cirilando <cirilando2244@gmail.com>
2024-06-01 23:11:14 +0530

    image

--- Client/src/Layout/layouts.jsx
+++ Client/src/Layout/layouts.jsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.png";
 
 const AuthLayout = ({ children }) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen ">
       <div className="flex items-center mb-4">
         <img className="w-16 md:w-20" src={logo} alt="Logo" />
         <p className="ml-2 text-lg md:text-xl font-bold font-serif">Talk Hub</p>
