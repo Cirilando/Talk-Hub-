3070f1ab76041d342315d064058beccb9a209060
Cirilando <cirilando2244@gmail.com>
2024-06-01 23:11:47 +0530

    changes

--- Client/src/Layout/layouts.jsx
+++ Client/src/Layout/layouts.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import logo from "../assets/logo.png";
+import React from 'react'
+import logo from "../assets/logo.png"
 
-const AuthLayout = ({ children }) => {
+const AuthLayout = ({children}) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen ">
-      <div className="flex items-center mb-4">
-        <img className="w-16 md:w-20" src={logo} alt="Logo" />
-        <p className="ml-2 text-lg md:text-xl font-bold font-serif">Talk Hub</p>
-      </div>
+    <>
+   <div className=" ml-[18px] md:ml-[680px] flex md:mt-1 mt-2  ">
+          <img
+            className="md:w-[90px] w-[50px] md:ml-0 ml-[100px]   "
+            src={logo}
+            alt="Logo"
+          />
+          <p className=" mt-2 md:mt-5 font-bold font-serif md:text-xl">
+            Talk Hub
+          </p>
+        </div>
       {children}
-    </div>
-  );
-};
+    </>
+  )
+}
 
 export default AuthLayout;
